b7c542343d8e61f8171e4f983a96c45695bf40ec
kamalexandre <63556679+kamalexandre@users.noreply.github.com>
2023-12-20 16:34:54 +0100

    Update viewer.mjs

--- pdfjs/web/viewer.mjs
+++ pdfjs/web/viewer.mjs
@@ -4513,7 +4513,7 @@ const defaultOptions = {
 }
 {
   defaultOptions.defaultUrl = {
-    value: "compressed.tracemonkey-pldi-09.pdf",
+    value: "git-cheat-sheet-education.pdf",
     kind: OptionKind.VIEWER
   };
   defaultOptions.disablePreferences = {
